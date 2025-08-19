5710616e0a2c84727ebf1d8ea7055b9961e15cdb
Abhishek Kumar <abhishek8579013@gmail.com>
2025-08-19 17:08:31 +0530

    update

--- backend/Middleware/upload.js
+++ backend/Middleware/upload.js
@@ -10,5 +10,9 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 50 * 1024 * 1024 } // 50 MB limit
+});
+
 module.exports = upload;
